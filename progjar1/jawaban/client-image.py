2c61008fa5ccaa422bff30b92adbc581d4bd9d3d
exceldeo <52096462+exceldeo@users.noreply.github.com>
2021-07-20 20:25:57 +0700

    memperbaiki

---
+++ progjar1/jawaban/client-image.py
@@ -0,0 +1,40 @@
+import sys
+import socket
+
+# Server Address
+ipserv = ['192.168.122.32', '192.168.122.83']
+
+count = 0
+for i in ipserv:
+        # Create a TCP/IP socket
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+        # Connect the socket to the port where the server is listening
+        server_address = (i, 10000)
+        print(f"connecting to {server_address}")
+        sock.connect(server_address)
+
+
+        try:
+                # Send data
+                img = "image.png"
+                imgfile = open(img, 'rb')
+                imgbytes = imgfile.read()
+                print(f"sending {img}")
+                sock.sendall(imgbytes)
+                # Look for the response
+                amount_received = 0
+                amount_expected = len(imgbytes)
+                count += 1
+                img_recv = "image-received-alpine" + str(count) + ".png"
+                with open(img_recv, 'wb') as file:
+                    while amount_received < amount_expected:
+                        data = sock.recv(16)
+                        amount_received += len(data)
+                        if not data:
+                            break
+                        file.write(data)
+        finally:
+                print(f"{img_recv} received")
+                print("closing")
+                sock.close()
