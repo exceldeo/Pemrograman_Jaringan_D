e64d4959adfbd02a01597b03b817b3db13dc8cad
exceldeo <52096462+exceldeo@users.noreply.github.com>
2021-07-21 10:24:26 +0700

    done

--- multi_process.py
+++ multi_process.py
@@ -1,24 +1,24 @@
-from library import download_gambar, get_url_list
+from file_client_cli import remote_get
 import time
 import datetime
 from multiprocessing import Process
 
-def download_semua():
+def download_pokijan():
     texec = dict()
-    urls = get_url_list()
     catat_awal = datetime.datetime.now()
-    for k in urls:
-        print(f"mendownload {urls[k]}")
+    nama_file = 'pokijan.jpg'
+    for k in range(100):
+        print(f"mendownload {nama_file} - {k}")
         waktu = time.time()
         #bagian ini merupakan bagian yang mengistruksikan eksekusi fungsi download gambar secara multiprocess
-        texec[k] = Process(target=download_gambar, args=(urls[k],))
+        texec[k] = Process(target=remote_get, args=(nama_file,))
         texec[k].start()
     #setelah menyelesaikan tugasnya, dikembalikan ke main process dengan join
-    for k in urls:
+    for k in range(100):
         texec[k].join()
     catat_akhir = datetime.datetime.now()
     selesai = catat_akhir - catat_awal
     print(f"Waktu TOTAL yang dibutuhkan {selesai} detik {catat_awal} s/d {catat_akhir}")
 #fungsi download_gambar akan dijalankan secara multi process
 if __name__=='__main__':
-    download_semua()
+    download_pokijan()
